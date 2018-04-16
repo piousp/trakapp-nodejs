44f8be74d221ddba8fa29428c6f32548ea8f9eff
Pablo Peraza <piousp@ciriscr.com>
2018-04-16 14:41:38 -0600

    Corregir las rutas de empleado

--- src/rest/rutas/empleado.js
+++ src/rest/rutas/empleado.js
@@ -1,55 +1,26 @@
 import express from "express";
 import D from "debug";
-import empleado from "../modelos/empleado.js";
-import funDB from "../comun-db.js";
-import { getID, deleteID, error } from "./_base";
+import modelo from "../modelos/empleado.js";
+import * as funBD from "./_base";
+
 
 const debug = D("ciris:rutas/empleado.js");
 
 const router = express.Router();
-const comun = new Comun(empleado);
 
-router.get("/:id", getID);
-router.get("/", getBase);
-router.put("/:id", putID);
+funBD.getID(router, modelo);
+funBD.getBase(router, modelo);
+funBD.putID(router, modelo);
+funBD.deleteID(router, modelo);
+
 router.post("/", postBase);
 
-function getID(req, res) {
-  comun.findOne(req.params.id)
-    .then(ok(res))
-    .catch(error(res));
-}
-
-function getBase(req, res) {
-  comun.find()
-    .then(ok(res))
-    .catch(error(res));
-}
-
-function putID(req, res) {
-  comun.findOneAndUpdate(req.params.id, req.body)
-    .then(ok(res))
-    .catch(error(res));
-}
-
 function postBase(req, res) {
+  debug("Post base");
   req.body.password = "movil123";
-  comun.create(req.body)
-    .then(ok(res))
-    .catch(error(res));
-}
-
-function ok(res) {
-  return obj => res.json(obj);
-}
-
-function error(res) {
-  return (err) => {
-    if (isEmpty(err)) {
-      return res.status(200).send({});
-    }
-    return res.status(500).send(err);
-  };
+  funBD(modelo).create(req.body)
+    .then(funBD.ok(res))
+    .catch(funBD.error(res));
 }
 
 export default router;
