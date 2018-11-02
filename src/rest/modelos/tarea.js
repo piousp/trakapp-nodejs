e2f0cf87991c7bfc3a95c5a6d13394edd0f86e22
ciris <fabio.flores@ciriscr.com>
2018-11-02 15:52:01 -0600

    feat: buscador de empleados

--- src/rest/modelos/tarea.js
+++ src/rest/modelos/tarea.js
@@ -23,6 +23,7 @@ const esquema = new mongoose.Schema({
   empleado: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "empleado",
+    required: true,
   },
   cuenta: {
     type: mongoose.Schema.Types.ObjectId,
